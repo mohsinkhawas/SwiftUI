74f0f32e8c7dd11d4d94b0f9e8e28b604a686d61
Mohsin Khawas <mokha.canada@gmail.com>
2025-06-06 22:19:14 -0400

    Changes on the window.

--- StandupTimer/StandupTimer/StandupTimerApp.swift
+++ StandupTimer/StandupTimer/StandupTimerApp.swift
@@ -6,37 +6,53 @@
 //
 
 import SwiftUI
+
+#if os(macOS)
 import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var windowTimer: Timer?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        windowTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] _ in
+            if let window = NSApplication.shared.windows.first {
+                window.level = .statusBar
+                window.backgroundColor = .white
+                window.isOpaque = true
+                window.hasShadow = true
+                window.isMovableByWindowBackground = true
+                window.standardWindowButton(.closeButton)?.isHidden = false
+                window.standardWindowButton(.miniaturizeButton)?.isHidden = true
+                window.standardWindowButton(.zoomButton)?.isHidden = true
+                window.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
+                window.hidesOnDeactivate = false
+            }
+        }
+    }
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        windowTimer?.invalidate()
+    }
+}
+#endif
 
 @main
 struct StandupTimerApp: App {
+    #if os(macOS)
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+            #if os(macOS)
+                .frame(width: 200, height: 150)
+            #endif
         }
+        #if os(macOS)
         .windowStyle(.hiddenTitleBar)
         .defaultSize(width: 200, height: 150)
         .windowResizability(.contentSize)
-    }
-}
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        if let window = NSApplication.shared.windows.first {
-            window.level = NSWindow.Level(rawValue: NSWindow.Level.floating.rawValue + 1)
-            window.backgroundColor = .white
-            window.isOpaque = true
-            window.hasShadow = true
-            window.isMovableByWindowBackground = true
-            window.standardWindowButton(.closeButton)?.isHidden = false
-            window.standardWindowButton(.miniaturizeButton)?.isHidden = true
-            window.standardWindowButton(.zoomButton)?.isHidden = true
-            
-            // Set window to be visible on all spaces and stay on top
-            window.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
-            window.hidesOnDeactivate = false
-        }
+        #endif
     }
 }
